6bfda7451eb87f5f12fd407e14edfaec3d1ceeb0
rickswordfire2032 <53525067+rickswordfire2032@users.noreply.github.com>
2020-07-07 10:52:33 +0300

    React-recipe-API

---
+++ src/heading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Heading = () =>{
+    return(
+        <div className="heading">
+            <h1>
+               Vice Munchies Recipes
+            </h1>
+        </div>
+    )
+}
+
+export default Heading;
